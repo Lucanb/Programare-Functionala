137419d70ecb47df9a0a9b13f47a65c126046885
Lucanb <lucanastasa@.com>
2024-03-29 11:54:33 +0200

    incercam --de inspectat si de lucrat aici

--- Lab/Lab5/lab5.hs
+++ Lab/Lab5/lab5.hs
@@ -61,6 +61,85 @@
 
 --0.2
 -- https://downloads.haskell.org/ghc/latest/docs/libraries/ghc-prim-0.11.0-62f6/GHC-Classes.html --> sunt scrise una sub alta aici.
---0.3
 
+--0.3
 data Nat = Cons [Bool] -- lista de valori de tip boolean
+
+--0.4
+instance Eq Nat where
+    (Cons a) == (Cons b) = a == b 
+
+instance Ord Nat where
+    (Cons a) <= (Cons b) = case compare (length a) (length b) of
+                                LT -> True
+                                GT -> False
+                                EQ -> compareBits a b
+                            where 
+                                compareBits [] [] = True   --aici fac recursia
+                                compareBits (x:hd) (y:tl)
+                                    |x<y = True
+                                    |x>y = False
+                                    |otherwise = compareBits hd tl    
+
+
+instance Integral Nat where
+
+    toInteger (Cons []) = 0
+    toInteger (Cons bits) = sum [if b then 2^i else 0 | (i, b) <- zip [0..] (reverse bits)]
+    quot (Cons []) _ = error "Nu se poate imparti la 0"
+    quot _ (Cons []) = error "Nu se poate imparti la 0"
+    quot (Cons a) (Cons b) = Cons (divide a b)
+        where
+            divide _ [] = [] 
+            divide [] _ = [] 
+            divide (False:as) (False:bs) = False : divide as bs
+            divide (True:as) (False:bs) = True : divide as bs
+            divide (False:as) (True:bs) = False : divide as (False:bs)
+            divide (True:as) (True:bs) = True : divide (zipWith (/=) as bs) bs
+
+    
+instance Num Nat where
+
+    (Cons a) + (Cons b) = Cons (addBits a b)
+        where
+            addBits [] bs = bs 
+            addBits as [] = as
+            addBits (x:hd) (y:tl) = (x /= y) : addBits hd tl
+    
+    (Cons a) * (Cons b) = Cons (multiplyBits a b)
+        where
+            multiplyBits as bs = foldr addZeroes [False | _ <- [1..length bs]] (map (\bit -> if bit then bs else [False | _ <- [1..length bs]]) as)
+            addZeroes = zipWith (/=)
+    
+    abs (Cons a) = Cons (a ++ [False])
+
+    signum (Cons a) = Cons (if any id a then [True] else [False])
+
+    fromInteger x = if x >= 0 then Cons (toBits x) else error "Negativ"
+        where
+            toBits 0 = []
+            toBits n = let (q, r) = n `quotRem` 2 in (r == 1) : toBits q
+    
+    negate (Cons a) = if any id a then Cons (map not a) else Cons a
+
+-- 0. 5    
+
+data Complex = Re[Int] | Im[Int] deriving(Show)
+
+---0.6 
+
+class MyOrd a where
+    f :: a -> a -> Bool --functie specifica
+
+instance MyOrd Int where
+    f x y = x <= y
+
+instance MyOrd a => MyOrd [a] where
+    f [] [] = True
+    f [] _ = True
+    f _ [] = False
+    f (x:hd) (y:tl) = if f x y then f hd tl else False
+
+sort :: MyOrd a => [a] -> [a]
+sort [] = []
+sort (x:hd) = sort [y | y <- hd, f y x] ++ [x] ++ sort [y | y <- hd, not (f y x)]
--- Lab/Lab5/notite.hs
+++ Lab/Lab5/notite.hs
@@ -37,3 +37,93 @@ instance Num Q where
     signum (Q n d) = Q (signum n * signum d) 1
     fromInteger x = Q (fromInteger x) 1
     negate (Q n d) = Q (negate n) d
+
+    -- data Nat = Cons [Bool] deriving (Show)
+
+-- -- Instanță Eq
+-- instance Eq Nat where
+--   (Cons xs) == (Cons ys) = xs == ys
+
+-- -- Instanță Ord
+-- instance Ord Nat where
+--   compare (Cons xs) (Cons ys) = compare (length xs) (length ys)
+
+-- -- Instanță Integral
+-- instance Integral Nat where
+--   toInteger (Cons xs) = sum [2^i | (i, bit) <- zip [0..] (reverse xs), bit]
+
+--   quotRem n d = quotRem' (toInteger n) (toInteger d)
+--     where
+--       quotRem' x y = (fromInteger (quot x y), fromInteger (rem x y))
+
+-- -- Instanță Num
+-- instance Num Nat where
+--   fromInteger n
+--     | n < 0     = error "Cannot represent negative numbers"
+--     | otherwise = Cons (toBinary n)
+--       where
+--         toBinary 0 = [False]
+--         toBinary 1 = [True]
+--         toBinary n = let (q, r) = n `quotRem` 2 in toBinary q ++ [r /= 0]
+  
+--   (+) = addNat
+--   (*) = multNat
+--   negate _ = error "Negation not defined for Nat"
+--   abs = id
+--   signum n = if n == 0 then 0 else 1
+
+-- -- Funcție auxiliară pentru adunarea a două numere naturale
+-- addNat :: Nat -> Nat -> Nat
+-- addNat (Cons xs) (Cons ys) = Cons (addBits xs ys False)
+--   where
+--     addBits [] [] False = []
+--     addBits [] [] True = [True]
+--     addBits (x:xs) [] carry = addBit x False carry : addBits xs [] (carry && not x)
+--     addBits [] (y:ys) carry = addBit False y carry : addBits [] ys (carry && not y)
+--     addBits (x:xs) (y:ys) carry = addBit x y carry : addBits xs ys (carry && (x || y))
+  
+--     addBit True True True = True
+--     addBit False False False = False
+--     addBit _ _ _ = True
+
+-- -- Funcție auxiliară pentru înmulțirea a două numere naturale
+-- multNat :: Nat -> Nat -> Nat
+-- multNat (Cons xs) (Cons ys) = sum [shift (fromInteger (toInteger (Cons [x])) i) | (i, x) <- zip [0..] xs]
+--   where
+--     shift m = m * fromInteger (toInteger (Cons ys))
+
+
+-- instance Integral Nat where
+--     -- 'quot' computes the integer division of two numbers.
+--     quot (Cons []) _ = error "Division by zero"
+--     quot _ (Cons []) = error "Division by zero"
+--     quot (Cons a) (Cons b) = Cons (quotHelper a b)
+--         where
+--             quotHelper a [] = []
+--             quotHelper [] _ = []
+--             quotHelper (False:as) (False:bs) = False : quotHelper as bs
+--             quotHelper (True:as) (False:bs) = True : quotHelper as bs
+--             quotHelper (False:as) (True:bs) = False : quotHelper as (False:bs)
+--             quotHelper (True:as) (True:bs) = True : quotHelper as (zipWith (/=) as bs)
+    
+--     -- 'rem' computes the remainder of the integer division of two numbers.
+--     rem (Cons []) _ = error "Division by zero"
+--     rem _ (Cons []) = error "Division by zero"
+--     rem (Cons a) (Cons b) = Cons (remHelper a b)
+--         where
+--             remHelper a [] = a
+--             remHelper [] _ = []
+--             remHelper (False:as) (False:bs) = remHelper as bs
+--             remHelper (True:as) (False:bs) = remHelper as bs
+--             remHelper (False:as) (True:bs) = False : remHelper as (False:bs)
+--             remHelper (True:as) (True:bs) = False : remHelper as (zipWith (/=) as bs)
+    
+--     -- 'div' computes the integer division of two numbers.
+--     div = quot
+    
+--     -- 'mod' computes the modulus (remainder) of the integer division of two numbers.
+--     mod = rem
+
+--     -- 'toInteger' converts a value of type 'Nat' to an 'Integer'.
+--     toInteger (Cons []) = 0
+--     toInteger (Cons bits) = sum [if b then 2^i else 0 | (i, b) <- zip [0..] (reverse bits)]
